f8897f16a2b4e244bd20b7689d8fcb98e95aa1c2
wilbert0838n <rohannadar88@gmail.com>
2026-01-11 17:30:58 +0530

    Stop tracking application.properties

---
+++ frontend/src/config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+
+// If you don't write this line, API_URL stays stuck inside this file!
+export default API_URL;
---
+++ src/main/java/contest_site/contest_site/config/ProblemSeeder.java
@@ -0,0 +1,4 @@
+package contest_site.contest_site.config;
+
+public class ProblemSeeder {
+}
